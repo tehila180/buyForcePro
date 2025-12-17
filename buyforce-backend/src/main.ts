2de7c06553edd3d68df6836974913e01b264d682
tehila.ch <tehila.ch12@gmail.com>
2025-12-17 22:09:40 +0200

    Update main.ts

--- buyforce-backend/src/main.ts
+++ buyforce-backend/src/main.ts
@@ -4,12 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ חובה כשיש Frontend ו-Backend על פורטים שונים
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://buyforce-web-j9ik.vercel.app',
+      'https://buyforce-web-j9ik-l88btrbro-tehilas-projects-7fdab0b7.vercel.app',
+    ],
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
