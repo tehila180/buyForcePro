5d88949a551e87a863f36f4fa6119f2569591c8c
tehila.ch <tehila.ch12@gmail.com>
2025-12-18 17:17:10 +0200

    Update main.ts

--- buyforce-backend/src/main.ts
+++ buyforce-backend/src/main.ts
@@ -7,13 +7,13 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://buyforce-web-j9ik.vercel.app',
-      'https://buyforce-web-j9ik-l88btrbro-tehilas-projects-7fdab0b7.vercel.app',
+      'https://buyforce-web-*.vercel.app',
+      'https://buyforce-web.vercel.app',
     ],
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
   await app.listen(process.env.PORT || 3001);
 }
+
 bootstrap();
