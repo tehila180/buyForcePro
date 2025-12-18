25e2edde20879594e8290ee02e76ce3950041ebc
tehila.ch <tehila.ch12@gmail.com>
2025-12-18 17:17:19 +0200

    Update paypal.client.ts

--- buyforce-backend/src/payments/paypal.client.ts
+++ buyforce-backend/src/payments/paypal.client.ts
@@ -1,16 +1,10 @@
-import * as paypal from '@paypal/checkout-server-sdk';
+import checkoutNodeJssdk from '@paypal/checkout-server-sdk';
 
 export function getPayPalClient() {
-  const env =
-    process.env.PAYPAL_ENV === 'live'
-      ? new paypal.core.LiveEnvironment(
-          process.env.PAYPAL_CLIENT_ID!,
-          process.env.PAYPAL_CLIENT_SECRET!
-        )
-      : new paypal.core.SandboxEnvironment(
-          process.env.PAYPAL_CLIENT_ID!,
-          process.env.PAYPAL_CLIENT_SECRET!
-        );
+  const env = new checkoutNodeJssdk.core.SandboxEnvironment(
+    process.env.PAYPAL_CLIENT_ID!,
+    process.env.PAYPAL_CLIENT_SECRET!
+  );
 
-  return new paypal.core.PayPalHttpClient(env);
+  return new checkoutNodeJssdk.core.PayPalHttpClient(env);
 }
