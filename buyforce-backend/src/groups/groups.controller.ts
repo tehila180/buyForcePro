69ca5e41017f5f0d9d6e44f2da7ddae6c48f9dd3
tehila.ch <tehila.ch12@gmail.com>
2025-12-22 13:56:09 +0200

    Update groups.controller.ts

--- buyforce-backend/src/groups/groups.controller.ts
+++ buyforce-backend/src/groups/groups.controller.ts
@@ -13,30 +13,33 @@ import { JwtAuthGuard } from '../auth/jwt.guard';
 export class GroupsController {
   constructor(private readonly groupsService: GroupsService) {}
 
+  // ⭐ קבוצות פתוחות לדף הבית
   @Get('featured')
   findFeatured() {
     return this.groupsService.findFeatured();
   }
 
+  // ⭐ הקבוצות שלי (מחייב התחברות)
   @UseGuards(JwtAuthGuard)
   @Get('my')
   getMyGroups(@Req() req: any) {
     return this.groupsService.findMyGroups(req.user.userId);
   }
 
+  // ⭐ הצטרפות לקבוצה (מחייב התחברות)
   @UseGuards(JwtAuthGuard)
   @Post(':id/join')
   joinGroup(@Param('id') id: string, @Req() req: any) {
     return this.groupsService.joinGroup(Number(id), req.user.userId);
   }
 
-  @UseGuards(JwtAuthGuard)
+  // ⭐ קבוצה אחת – ציבורי ❗
   @Get(':id')
-  getOne(@Param('id') id: string, @Req() req: any) {
-    return this.groupsService.findOne(Number(id), req.user.userId);
+  getOne(@Param('id') id: string) {
+    return this.groupsService.findOnePublic(Number(id));
   }
 
-  // (Admin)
+  // ⭐ ביטול קבוצה (Admin / מחייב התחברות)
   @UseGuards(JwtAuthGuard)
   @Post(':id/cancel')
   cancel(@Param('id') id: string) {
