7523c5d2bc7e7b6164690ddf739f5a66c8c16ac7
tehila.ch <tehila.ch12@gmail.com>
2025-12-22 14:11:10 +0200

    Update groups.controller.ts

--- buyforce-backend/src/groups/groups.controller.ts
+++ buyforce-backend/src/groups/groups.controller.ts
@@ -14,28 +14,37 @@ import { JwtAuthGuard } from '../auth/jwt.guard';
 export class GroupsController {
   constructor(private readonly groupsService: GroupsService) {}
 
+  // ⭐ קבוצות פתוחות לדף הבית
   @Get('featured')
   findFeatured() {
     return this.groupsService.findFeatured();
   }
 
+  // ⭐ קבוצות לפי מוצר (ציבורי)
+  @Get('product/:productId')
+  getByProduct(@Param('productId') productId: string) {
+    const id = Number(productId);
+    if (isNaN(id)) {
+      throw new BadRequestException('Product ID must be a number');
+    }
+    return this.groupsService.findByProduct(id);
+  }
+
+  // ⭐ הקבוצות שלי
   @UseGuards(JwtAuthGuard)
   @Get('my')
   getMyGroups(@Req() req: any) {
     return this.groupsService.findMyGroups(req.user.userId);
   }
 
+  // ⭐ הצטרפות לקבוצה
   @UseGuards(JwtAuthGuard)
   @Post(':id/join')
   joinGroup(@Param('id') id: string, @Req() req: any) {
-    const groupId = Number(id);
-    if (isNaN(groupId)) {
-      throw new BadRequestException('Group ID must be a number');
-    }
-    return this.groupsService.joinGroup(groupId, req.user.userId);
+    return this.groupsService.joinGroup(Number(id), req.user.userId);
   }
 
-  // ⭐ ציבורי ומוגן משגיאות
+  // ⭐ קבוצה אחת – ציבורי
   @Get(':id')
   getOne(@Param('id') id: string) {
     const groupId = Number(id);
@@ -45,13 +54,10 @@ export class GroupsController {
     return this.groupsService.findOnePublic(groupId);
   }
 
+  // ⭐ ביטול קבוצה
   @UseGuards(JwtAuthGuard)
   @Post(':id/cancel')
   cancel(@Param('id') id: string) {
-    const groupId = Number(id);
-    if (isNaN(groupId)) {
-      throw new BadRequestException('Group ID must be a number');
-    }
-    return this.groupsService.cancelGroup(groupId);
+    return this.groupsService.cancelGroup(Number(id));
   }
 }
