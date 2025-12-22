92465f4f2021e3f84d46bfa63d3af81ecf1087b8
tehila.ch <tehila.ch12@gmail.com>
2025-12-22 16:19:09 +0200

    Update groups.controller.ts

--- buyforce-backend/src/groups/groups.controller.ts
+++ buyforce-backend/src/groups/groups.controller.ts
@@ -9,6 +9,7 @@ import {
 } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { JwtAuthGuard } from '../auth/jwt.guard';
+import { OptionalJwtAuthGuard } from '../auth/optional-jwt.guard';
 
 @Controller('groups')
 export class GroupsController {
@@ -22,47 +23,26 @@ export class GroupsController {
   @Get('product/:productId')
   getByProduct(@Param('productId') productId: string) {
     const id = Number(productId);
-    if (isNaN(id)) throw new BadRequestException('Product ID must be a number');
+    if (isNaN(id)) throw new BadRequestException();
     return this.groupsService.findByProduct(id);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('my')
-  getMyGroups(@Req() req: any) {
-    return this.groupsService.findMyGroups(req.user.userId);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Post('create/:productId')
-  createGroup(@Param('productId') productId: string, @Req() req: any) {
-    const id = Number(productId);
-    if (isNaN(id)) throw new BadRequestException('Product ID must be a number');
-    return this.groupsService.createGroup(id, req.user.userId);
   }
 
   @UseGuards(JwtAuthGuard)
   @Post(':id/join')
   joinGroup(@Param('id') id: string, @Req() req: any) {
     const groupId = Number(id);
-    if (isNaN(groupId)) throw new BadRequestException('Group ID must be a number');
+    if (isNaN(groupId)) throw new BadRequestException();
     return this.groupsService.joinGroup(groupId, req.user.userId);
   }
 
-  // 👇 ציבורי + מחזיר מי המשתמש אם מחובר
+  // 👇 ציבורי + מזהה משתמש אם מחובר
+  @UseGuards(OptionalJwtAuthGuard)
   @Get(':id')
   getOne(@Param('id') id: string, @Req() req: any) {
     const groupId = Number(id);
-    if (isNaN(groupId)) throw new BadRequestException('Group ID must be a number');
+    if (isNaN(groupId)) throw new BadRequestException();
 
     const userId = req.user?.userId ?? null;
     return this.groupsService.findOnePublic(groupId, userId);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Post(':id/cancel')
-  cancel(@Param('id') id: string) {
-    const groupId = Number(id);
-    if (isNaN(groupId)) throw new BadRequestException('Group ID must be a number');
-    return this.groupsService.cancelGroup(groupId);
   }
 }
