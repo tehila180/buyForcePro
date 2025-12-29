515e98fd19e29387b0505857200150fa20c766e4
tehila.ch <tehila.ch12@gmail.com>
2025-12-29 14:08:11 +0200

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,18 +4,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
-import AppHeader from './components/AppHeader';
 import ProductsScreen from './screens/ProductsScreen';
 import ProductScreen from './screens/ProductScreen';
 import GroupScreen from './screens/GroupScreen';
 import MyGroupsScreen from './screens/MyGroupsScreen';
+import PayScreen from './screens/PayScreen';
+import CategoriesScreen from './screens/CategoriesScreen';
+import CategoryProductsScreen from './screens/CategoryProductsScreen';
+
+import AppHeader from './components/AppHeader';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"   // ⭐ הדף הראשון
+        initialRouteName="Home"
         screenOptions={({ navigation }) => ({
           header: () => <AppHeader navigation={navigation} />,
         })}
@@ -25,8 +30,14 @@ export default function App() {
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Products" component={ProductsScreen} />
         <Stack.Screen name="Product" component={ProductScreen} />
-<Stack.Screen name="Group" component={GroupScreen} />
-<Stack.Screen name="Profile" component={MyGroupsScreen} />
+        <Stack.Screen name="Group" component={GroupScreen} />
+        <Stack.Screen name="Profile" component={MyGroupsScreen} />
+        <Stack.Screen name="Pay" component={PayScreen} />
+        <Stack.Screen name="Categories" component={CategoriesScreen} />
+        <Stack.Screen
+          name="CategoryProducts"
+          component={CategoryProductsScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
