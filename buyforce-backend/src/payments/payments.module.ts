cdccd559a9473560b08f9187cb7dc959b1797ed1
tehila.ch <tehila.ch12@gmail.com>
2025-12-29 10:23:49 +0200

    Update payments.module.ts

--- buyforce-backend/src/payments/payments.module.ts
+++ buyforce-backend/src/payments/payments.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { PaypalService } from './paypal.service';
 
 @Module({
   controllers: [PaymentsController],
-  providers: [PaymentsService, PrismaService],
+  providers: [PaymentsService, PaypalService, PrismaService],
 })
 export class PaymentsModule {}
